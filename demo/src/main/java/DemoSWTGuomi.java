524b54e2b6905791f8f1109370fe8075a1538fd0
xdjiang <jiangxudong1982@gmail.com>
2021-09-27 18:26:47 +0800

    更新demo

--- demo/src/main/java/DemoSWTGuomi.java
+++ demo/src/main/java/DemoSWTGuomi.java
@@ -1,23 +1,22 @@
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jccdex.cert.JccCert;
 import com.jccdex.core.client.Wallet;
+import com.jccdex.core.client.WalletSM;
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
-public class DemoSWT {
+public class DemoSWTGuomi {
     public static void main(String[] args) {
         JccCert jccCert;
-        Wallet wallet1 = Wallet.fromSecret("snJzg27EHUxJj6fiHU1t7nJea5Y3Q");//jsXw3znEHPYDB78w1f21T6Ya3hUVVUwT6H
-        Wallet wallet2 = Wallet.fromSecret("snLpjnCcbG3QEEiGdvZAg6yBvMKHp");//jsys77BYKczqePgUA2S5oinctLtpd48qVP
+        WalletSM wallet1 = WalletSM.fromSecret("snHu1pSHRksTQQMJGa2gZH6gTSukK");//jpSojXsu7mfwStH7ig72yCg86ViKH5dHWN
+        WalletSM wallet2 = WalletSM.fromSecret("snwsVxeKqgKqckhomX6xvguwj5Jci");//j35gQsGrcsTPSmmRmxgVRwtetkp6NGvN4Z
 
         ArrayList<String> rpcNodes = new ArrayList <String> ();
-        rpcNodes.add("https://stestswtcrpc.jccdex.cn");
+        rpcNodes.add("http://139.198.19.157:4950");
         try {
             //初始化
-            jccCert = JccCert.init(wallet1.getSecret(), wallet2.getAddress(),false,rpcNodes);
+            jccCert = JccCert.init(wallet1.getSecret(), wallet2.getAddress(),true,rpcNodes);
 
             //文件存证
             System.out.println("文件存证上链------------------------------------------------------");
@@ -41,7 +40,7 @@ public class DemoSWT {
             //文件验真
             System.out.println("文件验真------------------------------------------------------");
             fis = new FileInputStream("/Users/xdjiang/Desktop/WechatIMG212.jpeg");
-            String txHash = "FF23599CC99D7A98BB2C7127A0994EA29CC2DB331DF3DEB381A7F78AA1A609A7";
+            String txHash = "E5622C3F704C4D9F4312E650DA1FC95A980AF05280701DA311B736E943466CE9";
             Boolean checkRet = jccCert.checkCert(fis,txHash);
             System.out.println("文件验真结果:"+checkRet);
             fis.close();
@@ -49,14 +48,14 @@ public class DemoSWT {
             //文本内容验真ss
             System.out.println("文本验真------------------------------------------------------");
             input = "测试字节流的存证上链哈希值";
-            txHash = "C4047C2A1A5AAE7C705956F1FB2B4FF084AF0534B2097BD97AEDFC8E079BACBD";
+            txHash = "2F9A4F58A18E7D2F47ABD096776411E956E729E76EEEC7CAD6ECF7AB1DADB502";
             checkRet = jccCert.checkCert(input.getBytes("UTF-8"),txHash);
             System.out.println("文本验真结果:"+checkRet);
 
             //hash验真
             System.out.println("哈希验真------------------------------------------------------");
             cHash = "3b77f64eb774969269555e7154a40ee137f431fa28d24dedcd25a77f54fef6bf";
-            txHash = "DF6938695AD9461F3CCB09D0DE18E45EA4591BA55AAF1A0E4776462E3A318EFA";
+            txHash = "FE93BDED70DBF9A6CC336F3BAE1FB2EB97173583E59E5C07964B3E78BA6E94FC";
             checkRet = jccCert.checkCert(cHash,txHash);
             System.out.println("哈希验真结果:"+checkRet);
         } catch (Exception e) {
